d199ddb94b4d5ae451878285aee187f1814a7996
namnvhustk61 <79648470+namnvhustk61@users.noreply.github.com>
2022-10-24 20:44:00 +0700

    .

--- test/TimeWarpScan/app/src/main/java/com/zero/hm/effect/timewarpscan/Listener.java
+++
@@ -1,9 +0,0 @@
-package com.zero.hm.effect.timewarpscan;
-
-public interface Listener {
-    public void initDone(int stage);
-
-    public void startAnimation();
-
-    public void imageSavedSuccessfully(String filePath);
-}
